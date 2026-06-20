f5101df5e119737a26c029cd0d49b2ec765d2d9d
Shivam Nath Goswami <goswamiji2006@gmail.com>
2026-06-20 11:33:58 +0530

    Add input prompts for better UX in booking flow

--- src/Main.java
+++ src/Main.java
@@ -23,21 +23,26 @@ public class Main {
 
             switch (input) {
                 case 1:
+                    System.out.println("These are the seats that are available");
                     show.showSeats();
                     break;
             
                 case 2:
+                    System.out.print("What is the seat row that you would like to book: ");
                     String row = scanner.next();
+                    System.out.print("what is the seat number that you would like to book: ");
                     int number = scanner.nextInt();
+                    
                     show.bookSeat(row, number);
+                    System.out.println("Have a nice show , your seat number is " + row + "-" +number);
                     break;
 
                 case 3:
-                    System.out.println("Good Bye");
+                    System.out.println("Good Bye and Have a nice day !!");
                     return;
 
                 default:
-                    System.out.println("Invalid");
+                    System.out.println("Invalid Statement");
             }
         }
     }
